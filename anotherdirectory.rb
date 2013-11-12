256bd1ef81814d82fdebb8a8b954c5c3cd2f4bfd
Michael <michaelnicholasfisher@gmail.com>
2013-11-12 16:36:02 +0000

    Two ways of printing arrays

---
+++ anotherdirectory.rb
@@ -0,0 +1,11 @@
+students = [
+	"terrence coverly",
+	'allan dicarra',
+	"oboe today",
+	"billy baldwin",
+]
+
+students.each{|student| puts student}
+
+
+puts "total is #{students.length}, donezo"
---
+++ directory.rb
@@ -0,0 +1,11 @@
+students = [
+	"terrence coverly",
+	'allan dicarra',
+	"oboe today",
+	"billy baldwin",
+]
+puts "here is a list of names"
+students.each do |student|
+	puts student
+end
+puts "total is #{students.length}, donezo"
